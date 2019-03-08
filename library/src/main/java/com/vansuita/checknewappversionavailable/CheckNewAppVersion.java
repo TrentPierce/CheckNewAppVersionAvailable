4e0819c0ced90fdfb04fe8920d399eae7082585c
Trent Pierce <Pierce.trent@gmail.com>
2019-03-08 10:01:28 -0600

    Update CheckNewAppVersion.java

--- library/src/main/java/com/vansuita/checknewappversionavailable/CheckNewAppVersion.java
+++ library/src/main/java/com/vansuita/checknewappversionavailable/CheckNewAppVersion.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 
